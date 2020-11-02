3bac529d48417df3d2c9253a27effa64ff42a7d5
NobleMathews <55821103+NobleMathews@users.noreply.github.com>
2020-11-03 00:16:33 +0530

    Lots

--- src/Pages/Lab.js
+++ src/Pages/Lab.js
@@ -1,9 +1,114 @@
-import React from "react";
+import React from 'react'
+import FadeIn from 'react-fade-in';
+import Footer from "../Components/Footer";
+import {Row,Container,Col,Image} from 'react-bootstrap';
+import img from "../assets/logo.png";
+import sir from "../assets/team/sir.jpg";
+import akhilla from "../assets/team/akhila.jpg";
+import darahas from "../assets/team/darahas.jpg";
+import dheeraj from "../assets/team/dheeraj.jpg";
+import eeshan from "../assets/team/eeshan.jpg";
+import karthik from "../assets/team/karthik.jpg";
+import kowndinya from "../assets/team/kowndinya.jpg";
+import noble from "../assets/team/noble.jpg";
+import shruti from "../assets/team/shruti.jpg";
+import shubhankar from "../assets/team/shubhankar.jpg";
+import vartika from "../assets/team/vartika.jpg";
+
 
 function Lab() {
+
   return (
-    <div>
+    <>
+    <FadeIn>
+      <div className="container pub-page-main">
+      <Container>
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={img} height="80px"/>
+        </Col>
+      </Row>
+      </Container>
+        <hr/>
+        <h5>People</h5>
+        <div className="container">
+        <div className="img-grid lab-member">
+          <figure className="figure">
+          <div className="img-wrap">
+            <img src={sir} alt="thumb" style={{width:"100%",height:"auto"}}/>
+          </div>
+            <figcaption class="figure-caption text-center">
+            <p>
+              <a href="https://www.cc.gatech.edu/~parikh/">Sridhar Chimalakonda</a>
+              <br/>
+              Assistant Professor
+            </p>
+            </figcaption>
+          </figure>
+
+          <figure className="figure">
+          <div className="img-wrap">
+            <img src={akhilla} alt="thumb" style={{width:"100%",height:"auto"}}/>
+          </div>
+            <figcaption class="figure-caption text-center">
+            <p>
+              <a href="https://www.cc.gatech.edu/~parikh/">Akhilla Manasa</a>
+              <br/>
+              Ph.D. Student
+            </p>
+            </figcaption>
+          </figure>
+
+          <figure className="figure">
+          <div className="img-wrap">
+            <img src={noble} alt="thumb" style={{width:"100%",height:"auto"}}/>
+          </div>
+            <figcaption class="figure-caption text-center">
+            <p>
+              <a href="https://www.cc.gatech.edu/~parikh/">Noble Mathews</a>
+              <br/>
+              B.tech. Student
+            </p>
+            </figcaption>
+          </figure>
+
+          <figure className="figure">
+          <div className="img-wrap">
+            <img src={akhilla} alt="thumb" style={{width:"100%",height:"auto"}}/>
+          </div>
+            <figcaption class="figure-caption text-center">
+            <p>
+              <a href="https://www.cc.gatech.edu/~parikh/">Devi Parikh</a>
+              <br/>
+              B.tech. Student
+            </p>
+            </figcaption>
+          </figure>
+
+          <figure className="figure">
+          <div className="img-wrap">
+            <img src={akhilla} alt="thumb" style={{width:"100%",height:"auto"}}/>
+          </div>
+            <figcaption class="figure-caption text-center">
+            <p>
+              <a href="https://www.cc.gatech.edu/~parikh/">Devi Parikh</a>
+              <br/>
+              B.tech. Student
+            </p>
+            </figcaption>
+          </figure>
+          
+        </div>
+        </div>
+        <hr/>
+        <h5>Past Students</h5>
+        <hr/>
+        <h5>Sponsors</h5>
+
     </div>
+    </FadeIn>
+    <Footer/>
+    </>
   );
 }
 
--- src/Pages/Publications.js
+++ src/Pages/Publications.js
@@ -1,7 +1,9 @@
 import React, { useRef } from 'react'
 import FadeIn from 'react-fade-in';
+import Footer from "../Components/Footer";
+import { animateScroll as scroll } from "react-scroll";
 
-const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)   
+const scrollToRef = (ref) => scroll.scrollTo(ref.current.offsetTop)   
 
 function Publication() {
 
@@ -9,24 +11,111 @@ function Publication() {
   const executeScroll = () => scrollToRef(myRef)
 
   return (
+    <>
     <FadeIn>
-      <div class="container pub-page-main">
+      <div className="container pub-page-main">
         <hr/>
         <div className="blog-post">
               <h2 className="blog-post-title">Latest Publications</h2>
-              <p className="blog-post-meta"><a className="preserve">[2020]</a> <a className="preserve">[2019]</a></p>
+              <p className="blog-post-meta"><a className="preserveb" onClick={()=>{executeScroll()}}>[2020]</a> <a className="preserveb">[2019]</a></p>
               <hr />
         </div>
-        <div class="row" ref={myRef}>
-          <div class="col-3">
-            <img src=""></img>
+
+        <h2>2020</h2>
+
+        <div className="row" ref={myRef}>
+          <div className="col-3 my-3">
+            <img className="img-thumbnail" src={"https://firebasestorage.googleapis.com/v0/b/risha-lab-server.appspot.com/o/h4.jpg?alt=media&token=3bc5fabd-ef32-4682-b8cf-b2160ae01cfc"} style={{width:"100%"}}></img>
           </div>
-          <div class="col-9">
-            2 of 3 (wider)
+          <div className="col-9" style={{display:"flex",alignItems:"center"}} >
+          <div className="container">
+            <div className="text-muted">
+              <a className="preserveb pr-2">[Noble Mathews]</a>
+              <a className="preserveb pr-2">[‪Akhila Venigalla]</a>
+              <a className="preserveb pr-2">[Sridhar Chimalakonda]</a>
+            </div>
+            <p><a className="preserveb font-weight-bold">With supporting text below as a natural lead-in to additional content.</a></p>
+            <div className="text-muted">
+              <a className="preserveb pr-2">[Github]</a>
+              <a className="preserveb pr-2">[Youtube]</a>
+              <a className="preserveb pr-2">[App]</a>
+            </div>
+          </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row" ref={myRef}>
+          <div className="col-3 my-3">
+            <img className="img-thumbnail" src={"https://firebasestorage.googleapis.com/v0/b/risha-lab-server.appspot.com/o/h4.jpg?alt=media&token=3bc5fabd-ef32-4682-b8cf-b2160ae01cfc"} style={{width:"100%"}}></img>
+          </div>
+          <div className="col-9" style={{display:"flex",alignItems:"center"}} >
+          <div className="container">
+            <div className="text-muted">
+              <a className="preserveb pr-2">[Noble Mathews]</a>
+              <a className="preserveb pr-2">[‪Akhila Venigalla]</a>
+              <a className="preserveb pr-2">[Sridhar Chimalakonda]</a>
+            </div>
+            <p><a className="preserveb font-weight-bold">With supporting text below as a natural lead-in to additional content.</a></p>
+            <div className="text-muted">
+              <a className="preserveb pr-2">[Github]</a>
+              <a className="preserveb pr-2">[Youtube]</a>
+              <a className="preserveb pr-2">[App]</a>
+            </div>
+          </div>
+          </div>
+        </div>
+
+        <hr/>
+
+        <div className="row" ref={myRef}>
+          <div className="col-3 my-3">
+            <img className="img-thumbnail" src={"https://firebasestorage.googleapis.com/v0/b/risha-lab-server.appspot.com/o/h4.jpg?alt=media&token=3bc5fabd-ef32-4682-b8cf-b2160ae01cfc"} style={{width:"100%"}}></img>
+          </div>
+          <div className="col-9" style={{display:"flex",alignItems:"center"}} >
+          <div className="container">
+            <div className="text-muted">
+              <a className="preserveb pr-2">[Noble Mathews]</a>
+              <a className="preserveb pr-2">[‪Akhila Venigalla]</a>
+              <a className="preserveb pr-2">[Sridhar Chimalakonda]</a>
+            </div>
+            <p><a className="preserveb font-weight-bold">With supporting text below as a natural lead-in to additional content.</a></p>
+            <div className="text-muted">
+              <a className="preserveb pr-2">[Github]</a>
+              <a className="preserveb pr-2">[Youtube]</a>
+              <a className="preserveb pr-2">[App]</a>
+            </div>
+          </div>
+          </div>
+        </div>
+
+        <hr/>
+
+        <div className="row" ref={myRef}>
+          <div className="col-3 my-3">
+            <img className="img-thumbnail" src={"https://firebasestorage.googleapis.com/v0/b/risha-lab-server.appspot.com/o/h4.jpg?alt=media&token=3bc5fabd-ef32-4682-b8cf-b2160ae01cfc"} style={{width:"100%"}}></img>
+          </div>
+          <div className="col-9" style={{display:"flex",alignItems:"center"}} >
+          <div className="container">
+            <div className="text-muted">
+              <a className="preserveb pr-2">[Noble Mathews]</a>
+              <a className="preserveb pr-2">[‪Akhila Venigalla]</a>
+              <a className="preserveb pr-2">[Sridhar Chimalakonda]</a>
+            </div>
+            <p><a className="preserveb font-weight-bold">With supporting text below as a natural lead-in to additional content.</a></p>
+            <div className="text-muted">
+              <a className="preserveb pr-2">[Github]</a>
+              <a className="preserveb pr-2">[Youtube]</a>
+              <a className="preserveb pr-2">[App]</a>
+            </div>
+          </div>
           </div>
         </div>
     </div>
     </FadeIn>
+    <Footer/>
+    </>
   );
 }
 
