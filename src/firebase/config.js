b9be81f93eb6006a4934820fcd43c79959017d96
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2022-04-16 13:32:18 +0530

    Remove Appcheck until web support

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,15 +15,15 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-const appCheck = firebase.appCheck();
-// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// key is the counterpart to the secret key you set in the Firebase console.
-appCheck.activate(
-  '6Lc8kXkfAAAAABe_YlCuTQBfKfLFAyQfDC-eSHRs',
+// const appCheck = firebase.appCheck();
+// // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// // key is the counterpart to the secret key you set in the Firebase console.
+// appCheck.activate(
+//   '6Lc8kXkfAAAAABe_YlCuTQBfKfLFAyQfDC-eSHRs',
 
-  // Optional argument. If true, the SDK automatically refreshes App Check
-  // tokens as needed.
-  true);
+//   // Optional argument. If true, the SDK automatically refreshes App Check
+//   // tokens as needed.
+//   true);
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
